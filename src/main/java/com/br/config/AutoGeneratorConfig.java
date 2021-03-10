4f6429522dd5ff95d1c793e5777d5f46d9356ef0
mayitbe65 <yu362172432@gmail.com>
2021-03-11 00:18:43 +0800

    1.代码生成器

--- src/main/java/com/br/config/AutoGeneratorConfig.java
+++ src/main/java/com/br/config/AutoGeneratorConfig.java
@@ -1,11 +1,18 @@
 package com.br.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.util.ArrayList;
 
 /**
  * @author yu.zhang
@@ -22,24 +29,20 @@ public class AutoGeneratorConfig {
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir");
         gc.setOutputDir(projectPath + "/src/main/java");
-
-        // 包配置
-//        gc.setControllerName("controller");
-//        gc.setEntityName("bean");
-//        gc.setServiceName("service");
-//        gc.setMapperName("mapper");
-
-        gc.setAuthor("yu.zhang");
-
+        gc.setAuthor("jesse");
         gc.setOpen(false);
         gc.setFileOverride(false);
-        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        gc.setServiceName("%sService");
+        gc.setIdType(IdType.ASSIGN_ID);
+        gc.setDateType(DateType.ONLY_DATE);
+        gc.setSwagger2(true);
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setUrl("jdbc:mysql://127.0.0.1:3306/mybatis_plus?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
+        dsc.setDbType(DbType.MYSQL);
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("");
@@ -47,29 +50,43 @@ public class AutoGeneratorConfig {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-//        pc.setModuleName();
-        pc.setParent("com.br.study");
+        pc.setModuleName("study");
+        pc.setParent("com.br");
         pc.setController("controller");
         pc.setService("service");
         pc.setEntity("bean");
         pc.setMapper("mapper");
         mpg.setPackageInfo(pc);
 
-
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
 //        strategy.setSuperEntityClass("你自己的父类实体,没有就不用设置!");
+        // 自动配置lombok
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
+        strategy.setControllerMappingHyphenStyle(true);
+
         // 公共父类
 //        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
         // 写于父类中的公共字段
-        strategy.setSuperEntityColumns("id");
+//        strategy.setSuperEntityColumns("id");
+        // 映射的表
         strategy.setInclude("user");
-        strategy.setControllerMappingHyphenStyle(true);
+
+        // 逻辑删除
+        strategy.setLogicDeleteFieldName("deleted");
+
+        // 自动填充配置
+        TableFill createTime = new TableFill("create_time", FieldFill.INSERT);
+        TableFill updateTime = new TableFill("update_time", FieldFill.INSERT_UPDATE);
+        ArrayList<TableFill> tableFills = new ArrayList<>();
+        tableFills.add(createTime);
+        tableFills.add(updateTime);
+        strategy.setTableFillList(tableFills);
         mpg.setStrategy(strategy);
+
         mpg.execute();
     }
 }
